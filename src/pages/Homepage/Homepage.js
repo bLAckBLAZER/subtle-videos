26d9797258ab3a8c80d68f7b6866c9c438deae6f
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2022-05-01 14:27:06 +0530

    minor fixes

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,9 +1,5 @@
 import "./Homepage.css";
 
 export const Homepage = () => {
-  return (
-    <div className="flex-1" style={{ backgroundColor: "#181818" }}>
-      Videos
-    </div>
-  );
+  return <div className="page">Videos</div>;
 };
