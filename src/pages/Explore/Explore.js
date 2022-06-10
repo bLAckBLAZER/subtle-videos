566d7d13f34083bbcc96a8efbf8668599a0dff04
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2022-06-10 17:46:10 +0530

    [fix] show message when no videos available

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -41,6 +41,9 @@ export const Explore = () => {
         ))}
       </section>
       <section className="grid-3-col">
+        {!filteredVideos.length && (
+          <div className=" h3 txt-center">Oops! We ran out of videos!.</div>
+        )}
         {filteredVideos.map((video) => (
           <VideoCard video={video} key={video._id} />
         ))}
